0d1764c778dba6d9451ebb1dbf68fbce453072d0
Sam Stokes <me@samstokes.co.uk>
2016-02-02 15:11:06 -0800

    Don't hardcode gh-pages branch in deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -21,7 +21,8 @@ fi
 
 BUILT_SITE=${1:?first argument should be the directory into which the site was built}
 [[ -d $BUILT_SITE ]] || error $BUILT_SITE is not a directory!
-REPO=${2:?second argument should be the git repo URL}
+REPO=${2:?second argument should be the git repo URL to push to}
+BRANCH=${3:?third argument should be the git branch to push to}
 
 DEPLOY_DIR=/tmp/deploy
 MESSAGE="Built from $SHORT_COMMIT: $CI_MESSAGE"
@@ -30,10 +31,10 @@ git config --global user.name "Robot (on behalf of $CI_COMMITTER_USERNAME)"
 git config --global user.email "$CI_COMMITTER_EMAIL"
 
 mkdir "$DEPLOY_DIR"
-git clone --branch=gh-pages "$REPO" "$DEPLOY_DIR"
+git clone --branch="$BRANCH" "$REPO" "$DEPLOY_DIR"
 
 rsync -rltvP --delete --exclude=.git "$BUILT_SITE"/ "$DEPLOY_DIR"/
 cd "$DEPLOY_DIR"
 git add -A
 git commit -m "$MESSAGE"
-git push origin HEAD:gh-pages
+git push origin HEAD:"$BRANCH"
